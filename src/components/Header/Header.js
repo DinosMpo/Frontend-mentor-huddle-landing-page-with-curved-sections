2685cd19a0990f765237379f81263290ed716bcf
DinosMpo <dinos7591@yahoo.gr>
2023-01-20 18:02:18 +0200

    header and footer components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+import TryIt from '../TryIt/TryIt';
+
+export default function Header() {
+	return (
+		<header>
+			<main>
+				<div>
+					<img src="../images/logo.svg" alt="icon message" />
+				</div>
+			</main>
+			<TryIt />
+		</header>
+	)
+}
