4998fa17cb6f2fc08dcfeea8e662f31afec3c6cd
DinosMpo <dinos7591@yahoo.gr>
2023-01-20 18:03:07 +0200

    app root files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import FirstSection from './components/FirstSection/FirstSection';
+import SecondSection from './components/SecondSection/SecondSection';
+import ThirdSection from './components/ThirdSection/ThirdSection';
+import FourthSection from './components/FourthSection/FourthSection';
+import FifthSection from './components/FifthSection/FifthSection';
+import SixthSection from './components/SixthSection/SixthSection';
+import Footer from './components/Footer/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <FourthSection />
+      <FifthSection />
+      <SixthSection />
+      <Footer />
+    </div>
+  )
 }
-
-export default App;
